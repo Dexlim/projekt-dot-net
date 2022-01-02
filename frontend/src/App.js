d5134a05b7b60015398b29e606807b86a0119436
Kasia Zaleska <kzaleska416@gmail.com>
2022-01-02 23:18:21 +0100

    fix layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import Loader from "react-loader-spinner";
 
-import Layout from "./components/Layout/Layout";
+import Layout from "./components/layout/Layout";
 
 const Home = React.lazy(() => import("./pages/Home"));
 const Menu = React.lazy(() => import("./pages/Menu"));
